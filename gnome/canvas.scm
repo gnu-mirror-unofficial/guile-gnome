7588b9b4895f8a6e7662abe19b0ac0bfcfd7c146
Jan Nieuwenhuizen <janneke@gnu.org>
2004-11-14 18:24:46 +0000

    Merge from janneke@gnu.org--2004-gnome Patches applied:
     * janneke@gnu.org--2004-gnome/libgnomecanvas--dev--0--base-0
       tag of guile-gnome-devel@gnu.org--2004/libgnomecanvas--dev--0--patch-8
    
     * janneke@gnu.org--2004-gnome/libgnomecanvas--dev--0--patch-1
       bezier example, add gnome-canvas-path-def wrapper

--- gnome/canvas.scm
+++ gnome/canvas.scm
@@ -28,4 +28,39 @@
   #:use-module (gnome gw canvas)
   #:use-module (gnome gw support modules))
 
+(use-modules (gnome gw canvas))
+
+(define-class <gnome-canvas-path-def> (<gobject>)
+  ;;(closure #:init-value (gnome-canvas-path-def-new)
+  (closure #:init-value (gnome-canvas-path-def-new)
+	   #:init-keyword #:path-def
+	   #:getter get-def #:setter set-def))
+
+(define-method (moveto (this <gnome-canvas-path-def>) x y)
+  (gnome-canvas-path-def-moveto (get-def this) x y))
+(define-method (curveto (this <gnome-canvas-path-def>) x1 y1 x2 y2 x3 y3)
+  (gnome-canvas-path-def-curveto (get-def this)  x1 y1 x2 y2 x3 y3))
+(define-method (lineto (this <gnome-canvas-path-def>) x y)
+  (gnome-canvas-path-def-lineto (get-def this) x y))
+(define-method (closepath (this <gnome-canvas-path-def>))
+  (gnome-canvas-path-def-closepath (get-def this)))
+(define-method (reset (this <gnome-canvas-path-def>))
+  (gnome-canvas-path-def-reset (get-def this)))
+
+(define -set-path-def set-path-def)
+(define -get-path-def get-path-def)
+
+(define-method (set-path-def (this <gnome-canvas-shape>)
+			     (def <gnome-canvas-path-def>))
+  (-set-path-def this (get-def def)))
+
+(define-method (get-path-def (this <gnome-canvas-shape>))
+  (make <gnome-canvas-path-def> #:path-def (-get-path-def this)))
+
+(export <gnome-canvas-path-def>
+	get-def set-def
+	moveto curveto lineto closepath reset
+	set-path-def get-path-def)
+
 (re-export-modules (gnome gw canvas))
+
