2e0838da633a54ee48fbe915dfda607c7909f766
Andy Wingo <wingo@pobox.com>
2006-11-05 20:15:18 +0100

    2006-11-05  Andy Wingo  <wingo@pobox.com>
    	* gnome/gw/libgnomeui-spec.scm: Use (gnome gw support g-wrap), the
    	g-wrap compatibility wrapper.

--- gnome/gw/libgnomeui-spec.scm
+++ gnome/gw/libgnomeui-spec.scm
@@ -26,8 +26,7 @@
 
 (define-module (gnome gw libgnomeui-spec)
   #:use-module (oop goops)
-  #:use-module (g-wrap)
-  #:use-module (g-wrap guile)
+  #:use-module (gnome gw support g-wrap)
   #:use-module (gnome gw gobject-spec)
   #:use-module (gnome gw gtk-spec)
   #:use-module (gnome gw support gobject)
