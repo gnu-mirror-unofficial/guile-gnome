428e8ba333507c37984445b66f80ba539dfeb575
Andy Wingo <wingo@pobox.com>
2007-08-28 00:08:07 +0200

    2007-08-28  Andy Wingo  <wingo@pobox.com>
    	* gnome/gobject/generics.scm: Rewrite to export the generics
    	normally instead of molesting the root module.

--- gnome/gobject/generics.scm
+++ gnome/gobject/generics.scm
@@ -20,26 +20,65 @@
 
 ;;; Commentary:
 ;;
-;;Generic functions for procedures in the (gnome gobject) module.
+;;Generic functions for procedures in the (gnome gobject) module. Say
+;;something here about the nature of generics in guile-gnome
 ;;
 ;;; Code:
 
 (define-module (gnome gobject generics)
-  :use-module (gnome gobject)
-  :use-module (oop goops))
+  #:use-module (gnome gobject utils)
+  #:use-module (gnome gobject)
+  #:use-module (oop goops)
+  #:export (get set emit connect connect-after block unblock
+            disconnect connected? invoke create-signal get-signals
+            get-properties get-property-names find-property))
 
-(define exports '(get set emit connect connect-after block unblock
-                  disconnect connected? equal? invoke create-signal get-signals
-                  get-properties get-property-names find-property))
+(define-generic-with-docs get
+  "A shorthand for @code{gobject-get-property}.")
 
-;; make sure we have generic methods
-(for-each
- (lambda (sym)
-   (let* ((existing-var (module-variable (current-module) sym))
-          (existing-val (if existing-var (variable-ref existing-var) #f)))
-     (module-add! (current-module) sym (make-variable 
-                                        (ensure-generic existing-val sym)))))
- exports)
+(define-generic-with-docs set
+  "A shorthand for @code{gobject-set-property}.")
+
+(define-generic-with-docs emit
+  "A shorthand for @code{gtype-instance-signal-emit}.")
+
+(define-with-docs connect
+  "A shorthand for @code{gtype-instance-signal-connect}, which also
+defaults to the core Guile definition of @code{connect}."
+  (ensure-generic (module-ref the-scm-module 'connect)))
+
+(define-generic-with-docs connect-after
+  "A shorthand for @code{gtype-instance-signal-connect-after}.")
+
+(define-generic-with-docs block
+  "A shorthand for @code{gsignal-handler-block}.")
+
+(define-generic-with-docs unblock
+  "A shorthand for @code{gsignal-handler-unblock}.")
+
+(define-generic-with-docs disconnect
+  "A shorthand for @code{gsignal-handler-disconnect}.")
+
+(define-generic-with-docs connected?
+  "A shorthand for @code{gsignal-handler-connected?}")
+
+(define-generic-with-docs invoke
+  "A shorthand for @code{gclosure-invoke}.")
+
+(define-generic-with-docs create-signal
+  "A shorthand for @code{gtype-class-create-signal}.")
+
+(define-generic-with-docs get-signals
+  "A shorthand for @code{gtype-class-get-signals}.")
+
+(define-generic-with-docs get-properties
+  "A shorthand for @code{gobject-class-get-properties}.")
+
+(define-generic-with-docs get-property-names
+  "A shorthand for @code{gobject-class-get-property-names}.")
+
+(define-generic-with-docs find-property
+  "A shorthand for @code{gobject-class-find-property}.")
 
 (define-method (get (object <gobject>) (name <symbol>))
   (gobject-get-property object name))
@@ -48,9 +87,8 @@
   (gobject-set-property object name value))
 
 (define-method (emit (object <gtype-instance>) (name <symbol>) . args)
-  (apply gtype-instance-signal-emit (append (list object name) args)))
+  (apply gtype-instance-signal-emit object name args))
 
-;; This one shadows 'connect' from (guile). Oh well!
 (define-method (connect (object <gtype-instance>) (name <symbol>) (func <procedure>))
   (gtype-instance-signal-connect object name func))
 
@@ -73,7 +111,7 @@
   (eq? (slot-ref o1 'gtype-instance) (slot-ref o2 'gtype-instance)))
 
 (define-method (invoke (closure <gclosure>) . args)
-  (apply gclosure-invoke (cons closure args)))
+  (apply gclosure-invoke closure args))
 
 (define-method (create-signal (class <gtype-class>) (name <symbol>) return-type param-types)
   (gtype-class-create-signal class name return-type param-types))
@@ -92,9 +130,3 @@
 
 (define-method (find-properties (class <gtype-class>))
   (gobject-class-find-properties class))
-
-;; Put these generics in the root module for all to enjoy
-(for-each
- (lambda (sym)
-   (module-define! the-root-module sym (module-ref (current-module) sym)))
- exports)
