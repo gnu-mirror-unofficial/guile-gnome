6b5bb80e3f4b519fb04626835e03640536c7829e
Andy Wingo <wingo@pobox.com>
2007-09-24 23:12:59 +0200

    * test-suite/Makefile.am: Update for gobject docs moving.
    	* doc/gobject/docs.mk: Update a bit.
    
    	* test-suite/gobject.api: Regenerate for gtk-doc.scm changes, an
    	incompatible change but not in any released version.

--- doc/gobject/docs.mk
+++ doc/gobject/docs.mk
@@ -2,12 +2,10 @@ clean-docs:
 	rm -f $(doc).texi
 	rm -f $(doc).info
 	rm -f $(doc)scmfiles
-	rm -f html-stamp
-	rm -rf html
 	rm -f $(addprefix $(doc).,aux cp cps fn fns ky log pdf pg pg toc tp tps vr vrs)
 	rm -rf $(doc).html
 
-EXTRA_DIST=$(doc).scm make-texinfo.scm make-html.scm docs.mk
+EXTRA_DIST=$(doc).scm make-texinfo.scm docs.mk
 DISTCLEANFILES=$(doc).texi $(doc)scmfiles
 
 $(doc)scmfiles: $(doc).scm
@@ -19,9 +17,3 @@ depfiles=$(addprefix $(srcdir)/../../,$(shell test ! -f $(doc)scmfiles || cat $(
 $(doc).texi: $(srcdir)/$(doc).scm $(doc)scmfiles $(depfiles)
 	$(top_builddir)/dev-environ $(srcdir)/make-texinfo.scm $(srcdir)/$(doc).scm $(DOT_DOC_FILES) >$@.tmp
 	mv $@.tmp $@
-
-html: html-stamp $(srcdir)/$(doc).scm $(depfiles)
-html-stamp: $(scm-module-files)
-	$(top_srcdir)/dev-environ $(srcdir)/make-html.scm $(srcdir)/$(doc).scm
-	touch $@
-
